5be91d1978726d3f921c2a2eb41b5ef8971cc7e6
amitayt <amitay.tsinis@gigaspaces.com>
2024-07-31 15:25:36 +0300

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 export default function Home() {
     return (
         <>
-            <h2>this is a test</h2>
+            <h2>this is a testaa</h2>
         <h1> welcome home</h1>
         <Link href='/blog'>Blog</Link>
         </>
